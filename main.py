43cc7cc269c0e9d4de6af2b1c6f88a25792dd2a9
Ayushi Rawat <ayushirawat4469@gmail.com>
2026-07-09 13:26:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 #1 for snake
 #0 for water 
 #-1 for gun
@@ -6,14 +7,14 @@ your_score=0
 computer_score=0
 draw_score=0
 next_choice='y'
+
 while next_choice== 'y':
     
     for i in range(0,5):
-        import random
 
         computer= random.choice([1,0,-1])
 
-        yourChoice=input("enter your character : ")
+        yourChoice=input("enter your character : ").lower()
         youDict={'s':1 ,'w':0 , 'g':-1}
         revDict={1:'snake',0:'water', -1:'gun'}
 
@@ -51,15 +52,15 @@ while next_choice== 'y':
         if i!=4:
             print("..........Next round..........") 
         else:
-            print("..........Result time.........")
+            print("||..........Result time.........||")
     print(f"your total score : {your_score}")
     print(f"computer's total score : {computer_score}")
     print(f'total no. of draws : {draw_score}')
             
     if(your_score>computer_score):
-        print(":)...you are the winner...:)")
+        print("\n:)...you are the winner...:)\n")
     elif(computer_score>your_score):
-            print(":)...computer is the winner...:)")
+            print("\n:)...computer is the winner...:)\n")
     else:
             print("It's a tie") 
     next_choice= input("Do you want to play (Y/N)?").lower()
